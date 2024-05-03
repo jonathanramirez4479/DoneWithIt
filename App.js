aeb0ba8bb3482a28824589ab552711a8456b8022
jonathanramirez4479 <106727963+jonathanramirez4479@users.noreply.github.com>
2024-05-03 14:33:46 -0700

    still working on tutorial

--- App.js
+++ App.js
@@ -2,9 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  console.log("new ")
+
   return (
     <View style={styles.container}>
-      <Text>Jonathan!</Text>
+      <Text>This is my app!</Text>
       <StatusBar style="auto" />
     </View>
   );
