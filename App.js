452025c02d339ae46eac8475f7683f1658cc7cb6
jonathanramirez4479 <106727963+jonathanramirez4479@users.noreply.github.com>
2024-05-03 19:39:38 -0700

    ignore

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-
+  let x;
   console.log("new ")
 
   return (
